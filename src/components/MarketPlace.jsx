8e6807bb0a30108b10366b11ea49b3bb7265d71e
LucasHeernan <lhbenitez2@gmail.com>
2022-10-26 12:50:33 -0300

    first commit

---
+++ src/components/MarketPlace.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from 'react-native';
+
+function MarketPlace() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>This is the Market!</Text>
+      </View>
+    );
+}
+
+export default MarketPlace
---
+++ src/components/Profile.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { View, Text, StyleSheet, ScrollText, Button } from 'react-native';
+import useAxios from 'axios-hooks';
+  
+function Profile() {
+
+    const [{ data, loading, error }, refetch] = useAxios('https://reqres.in/api/users?delay=1'
+        // {
+        //     manual: true,
+        // }
+    )
+
+    if (loading) return <Text>Loading...</Text>
+    if (error) return <ScrollText>{JSON.stringify(error, null, 2)}</ScrollText>
+
+    return (
+        <View style={styles.container}>
+          <Button onPress={()=> refetch({
+            data: {
+              delay: 2
+            }
+          })} title="Refetch" />
+          <Text style={styles.paragraph}>{JSON.stringify(data, null, 2)}</Text>
+        </View>
+    );  
+    // return (
+    //     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    //         <Text>This is the Profile!</Text>
+    //     </View>
+    // );
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      backgroundColor: '#ecf0f1',
+      padding: 8,
+    },
+    paragraph: {
+      marginTop: 8,
+    },
+});
+
+export default Profile
