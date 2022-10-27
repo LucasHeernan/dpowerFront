947bbc32f947e2b2bbf2001eda036116f25b8e1b
jorgechdea <jorgechdea@gmail.com>
2022-10-27 17:42:04 -0500

    primer commit homescreen

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { View, Text, StyleSheet, ScrollText, Button, ScrollView } from 'react-native';
 import useAxios from 'axios-hooks';
+
   
 function Profile() {
 
@@ -9,19 +10,38 @@ function Profile() {
         //     manual: true,
         // }
     )
+    
 
     if (loading) return <Text>Loading...</Text>
     if (error) return <ScrollText>{JSON.stringify(error, null, 2)}</ScrollText>
+    
+
+    const perfiles2 = data
+    console.log("esto es lo que tiene data..........."+data.data.length)
+    
 
     return (
+        // <ScrollView contentContainerStyle={styles.container}>
+        //   {/* <Button onPress={()=> refetch({
+        //     data: {
+        //       delay: 2
+        //     }
+        //   })} title="Refetch" /> */}
+        //   <Text style={styles.paragraph}>{perfiles2}</Text>
+        // </ScrollView>
+
         <ScrollView>
-          <Button onPress={()=> refetch({
-            data: {
-              delay: 2
-            }
-          })} title="Refetch" />
-          <Text style={styles.paragraph}>{JSON.stringify(data, null, 2)}</Text>
+           
+                <View >
+               
+              <Text>Hola</Text>
+                
+                </View>
+ 
+        
+       
         </ScrollView>
+
     );  
     // return (
     //     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
@@ -32,14 +52,30 @@ function Profile() {
 
 const styles = StyleSheet.create({
     container: {
-      flex: 1,
       justifyContent: 'center',
-      backgroundColor: '#ecf0f1',
+      backgroundColor: "#7D7D7D",
       padding: 8,
     },
     paragraph: {
       marginTop: 8,
     },
+    title:{
+      marginTop: 16,
+      paddingVertical: 8,
+      borderWidth: 4,
+      borderColor: "#20232a",
+      borderRadius: 6,
+      backgroundColor: "#C7D31E",
+      color: "000000",
+      textAlign: "center",
+      fontSize: 30,
+      fontWeight: "bold"
+    },
+    tinyLogo: {
+      width: 50,
+      height: 50,
+    },
+    
 });
 
 export default Profile
