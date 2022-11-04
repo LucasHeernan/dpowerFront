bc9608bce993e369425094f4894d92676faac3fa
LucasHeernan <lhbenitez2@gmail.com>
2022-11-04 12:40:04 -0300

    Cambio ruta back

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,15 +1,11 @@
 import axios from 'axios';
 import { GET_ALL_PRODUCTS, GET_PRODUCT_BY_ID, GET_CATEGORIES, ORDER_BY_PRICE, ORDER_BY_NAME,
 FILTER_BY_CATEGORY, GET_PRODUCT_BY_NAME, CLEAR_MARKET, LOGIN_DATA } from '../actionTypes';
-import { MY_IP } from '@env';
-
-const IP = MY_IP;
 
 export function getAllProducts() {
     return async (dispatch) => {
         try {
-            // const data = await axios(`http://${IP}:3001/products`).then(e => e.data);
-            const data = await axios(`http://192.168.0.77:3001/products`).then(e => e.data);
+            const data = await axios(`https://dpower-production.up.railway.app/products`).then(e => e.data);
             return dispatch({
                 type: GET_ALL_PRODUCTS,
                 payload: data
@@ -23,8 +19,7 @@ export function getAllProducts() {
 export function getProductById(id) {
     return async (dispatch) => {
         try {
-            // const data = await axios(`https://${IP}:3001/products/${id}`).then(e => e.data);
-            const data = await axios(`https://192.168.0.77:3001/products/${id}`).then(e => e.data);
+            const data = await axios(`https://dpower-production.up.railway.app/products/${id}`).then(e => e.data);
             return dispatch({
                 type: GET_PRODUCT_BY_ID,
                 payload: data
@@ -38,8 +33,7 @@ export function getProductById(id) {
 export function getCategories() {
     return async (dispatch) => {
         try {
-            // const data = await axios(`http://${IP}:3001/products`).then(e => e.data);
-            const data = await axios(`http://192.168.0.77:3001/products`).then(e => e.data);
+            const data = await axios(`https://dpower-production.up.railway.app/products`).then(e => e.data);
             return dispatch({
                 type: GET_CATEGORIES,
                 payload: data
