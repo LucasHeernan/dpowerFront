df1807e537f9cf73b56597438621400d75b37a6e
IgnacioVivas <ignacab@hotmail.com>
2022-11-14 18:57:46 -0300

    incorporando stripe, componente checkoutForm

--- App.js
+++ App.js
@@ -1,17 +1,21 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Navigation from './Navigation'
+import React from 'react';
+import Navigation from './Navigation';
 import { Provider } from 'react-redux';
 import { store, persistor } from './src/redux';
-import { PersistGate } from 'redux-persist/integration/react'
-import { Text } from "react-native";
+import { PersistGate } from 'redux-persist/integration/react';
+import { Text } from 'react-native';
+import { StripeProvider } from '@stripe/stripe-react-native';
+import CheckoutForm from './src/components/CheckoutForm';
 
 export default function App() {
   return (
-      <Provider store={store}>
-        <PersistGate loading={<Text>Loading...</Text>} persistor={persistor}>
-          <Navigation/>
-        </PersistGate>
-      </Provider>
+    <Provider store={store}>
+      <PersistGate loading={<Text>Loading...</Text>} persistor={persistor}>
+        <Navigation />
+      </PersistGate>
+      {/* <StripeProvider publishableKey='pk_test_51M2e4QDkxUT8dvAmJVln7ly2JTzdSGAKTwKmoj5Oo9azUxYohYz6upz0ER8JboiIDVMOgFzzgd0CLQbJlKYjoMqm00wXrVxeUY'>
+        <CheckoutForm />
+      </StripeProvider> */}
+    </Provider>
   );
 }
